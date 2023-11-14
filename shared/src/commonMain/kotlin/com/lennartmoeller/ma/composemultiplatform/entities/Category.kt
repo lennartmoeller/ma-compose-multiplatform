5ed26422486cd37472d7cb4d2807f6ae6f9ff827
Lennart Möller <kontakt@lennartmoeller.com>
2023-11-14 19:07:57 +0100

    E11 Add category creating and editing

--- shared/src/commonMain/kotlin/com/lennartmoeller/ma/composemultiplatform/entities/Category.kt
+++ shared/src/commonMain/kotlin/com/lennartmoeller/ma/composemultiplatform/entities/Category.kt
@@ -5,18 +5,18 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Category(
-    @SerialName("id") var id: Int,
+    @SerialName("id") var id: Int = 0,
     @SerialName("label") var label: String,
     @SerialName("type") var type: Int,
-    @SerialName("icon") var icon: String?,
+    @SerialName("icon") var icon: String? = null,
 ) {
     companion object {
         fun fromMap(map: Map<String, Any>): Category {
             return Category(
-                id = map["id"] as Int,
+                id = map["id"] as? Int ?: 0,
                 label = map["label"] as String,
                 type = map["type"] as Int,
-                icon = map["icon"] as String
+                icon = map["icon"] as String?,
             )
         }
     }
@@ -25,6 +25,6 @@ data class Category(
         map["id"]?.let { id = it as Int }
         map["label"]?.let { label = it as String }
         map["type"]?.let { type = it as Int }
-        map["icon"]?.let { icon = it as String }
+        map["icon"]?.let { icon = it as String? }
     }
 }
